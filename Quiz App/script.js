7916c4484b9f33dd8c5bd70d09c5d3a6d37d571e
Şevval Buğdaycı <sevval.bugdayci@btsgrp.com>
2022-10-28 15:14:38 +0300

    QuizApp

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -13,10 +13,10 @@ Soru.prototype.cevabiKontrolEt = function(cevap) {
 
 
 let sorular = [
-    new Soru("1-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c"),
-    new Soru("2-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c"),
-    new Soru("3-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c"),
-    new Soru("4-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c")
+    new Soru("1-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm",d:"Nuget"},"c"),
+    new Soru("2-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm",d:"Nuget"},"c"),
+    new Soru("3-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm",d:"Nuget"},"c"),
+    new Soru("4-Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm",d:"Nuget"},"c")
 ]
 
 function Quiz(sorular) {
@@ -37,14 +37,37 @@ console.log(quiz.soruGetir());
 
 
 document.querySelector(".btn_start").addEventListener("click", function () {
+    document.querySelector(".quiz_box").classList.add("active");
+    soruGoster(quiz.soruGetir());
 
-    if(quiz.sorular.lenght != quiz.soruIndex) {
+
+})
+
+document.querySelector(".next_btn").addEventListener("click" , function () {
+    if(quiz.sorular.lenght != quiz.soruIndex + 1) {
 
         document.querySelector(".quiz_box").classList.add("active");
-        console.log(quiz.soruGetir());
+        soruGoster(quiz.soruGetir())
         quiz.soruIndex += 1;
     } else {
         console.log("Quiz Bitti");
     }
-
 })
+
+function soruGoster(soru) {
+    let question = `<span>${soru.soruMetni}</span>`;
+    let options = ``;
+
+    for (let cevap in soru.cevapSecenekleri) {
+        options +=
+            `
+             <div class="option">
+               <span> <b>${cevap}</b> : ${soru.cevapSecenekleri[cevap]}</span>
+             </div>
+            `;
+    }
+
+    document.querySelector(".question_text").innerHTML = question;
+    document.querySelector(".option_list").innerHTML = options;
+
+}
